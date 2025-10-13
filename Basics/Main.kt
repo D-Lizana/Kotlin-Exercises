8638c5d15a15f4bff94f38a580e7b1533bb92c2e
DLizanaDev <Helgait@usal.es>
2025-10-13 19:27:30 +0200

    Variables and Constants

---
+++ Basics/Main.kt
@@ -0,0 +1,19 @@
+class Main {
+
+
+
+
+
+}
+
+private fun variablesYconstantes(){
+    // Variables
+    var miVariable = "Se puede cambiar"
+    println(miVariable)
+
+    // Constantes
+    val miConstante = "No se puede cambiar"
+    val segundaConstante = miVariable
+    println(segundaConstante)
+}
+
