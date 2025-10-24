dad8e931dd859430cc03283ec1c9016b3a5aadaa
DLizanaDev <Helgait@usal.es>
2025-10-24 12:06:58 +0200

    Solved exercise 2

--- Basics/src/Main.kt
+++ Basics/src/Main.kt
@@ -1,8 +1,10 @@
+import java.util.*
+
 fun main(){
 
     ejercicio1("Efren", 30)
 
-
+    ejercicio2(descuento = 0.3, precio = 100)
 
 }
 
@@ -11,4 +13,15 @@ fun ejercicio1(nombre: String?, edad: Int){
     val edad: Int = edad
 
     println("Hola ${nombre?: "Usuario desconocido"} tienes $edad años.")
+}
+
+fun ejercicio2(precio: Int, descuento: Double = 0.1, aplicarIva: Boolean = true): Double{
+
+    var precioFinal = precio - (precio*descuento)
+
+    if(aplicarIva){
+        precioFinal *= 1.21
+    }
+
+    return precioFinal
 }
