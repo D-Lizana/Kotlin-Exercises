0f5e3d82dd255e46080d55cb18c5a8c7861f4486
DLizanaDev <Helgait@usal.es>
2025-10-26 20:49:05 +0100

    Created vehicle and car classes for exercise 4

---
+++ Basics/src/Coche.kt
@@ -0,0 +1,7 @@
+class Coche(marca: String): Vehiculo(marca) {
+
+    override fun acelerar(){
+        println("El coche esta acelerando")
+    }
+
+}
---
+++ Basics/src/Vehiculo.kt
@@ -0,0 +1,7 @@
+open class Vehiculo(val marca: String){
+
+    open fun acelerar(){
+        println("El vehiculo esta acelerando.")
+    }
+
+}
