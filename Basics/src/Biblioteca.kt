02ee21f953d7829c7a57a89daff1b199ad57a807
DLizanaDev <Helgait@usal.es>
2025-10-27 11:26:34 +0100

    implemented methods for library

--- Basics/src/Biblioteca.kt
+++ Basics/src/Biblioteca.kt
@@ -20,6 +20,36 @@ class Biblioteca(var libros: ArrayList<Libro> = arrayListOf()) {
         return false
     }
 
+    fun devolverLibro(titulo: String): Boolean {
+        for (l in libros) {
+            if (l.titulo === titulo && !l.disponible) {
+                l.disponible = true
+                println("Libro devuelto.")
+                return true
+            }
+        }
+
+        println("Libro no encontrado.")
+        return false
+    }
+
+    fun listarLibros(){
+        for (l in libros){
+            l.toString()
+        }
+        println()
+    }
+
+    fun buscarPorAutor(autor: String): List<Libro>{
+        var lista: MutableList<Libro> = mutableListOf()
+        for (l in libros){
+            if(l.autor === autor){
+                lista.add(l)
+            }
+        }
+
+        return lista
+    }
 
 
 }
--- Basics/src/Libro.kt
+++ Basics/src/Libro.kt
@@ -1,5 +1,8 @@
 class Libro(val titulo: String, val autor: String, var disponible: Boolean) {
 
+    override fun toString(): String {
+        return "Titulo: $titulo, Autor: $autor, Disponible: $disponible."
+    }
 
 
 }
